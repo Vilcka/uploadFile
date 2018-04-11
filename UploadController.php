d6da4dfe1daddaec2ee323b2f262e4f927f678c4
Gauthier Arnaud <Vilc86@hotmail.com>
2018-04-11 19:53:34 +0200

    yolo

--- UploadController.php
+++ UploadController.php
@@ -13,25 +13,14 @@ class UploadController {
         'png' => 'image/png',
         'gif' => 'image/gif',
     ];
-    private $extension;
-    private $fileName;
-    private $tmpFilesPath;
 
-
-
-
-    public function __construct($fileName)
-    {
-        $this->fileName = $fileName;
-        $this->extension = $this->setExtension($fileName);
-
-    }
 
     public function uniqueFileName()
     {
         $uniqueFileName = 'image' . uniqid() . '.' . self::getExtension();
         return $uniqueFileName;
     }
+
     public function setExtension($fileName)
     {
         $extension = pathinfo($fileName, PATHINFO_EXTENSION);
@@ -39,43 +28,20 @@ class UploadController {
 
     }
 
-    /**
-     * @return mixed
-     */
-    public function getExtension()
+    public function checkExtension($fileName)
     {
-        return $this->extension;
-    }
-
-
-
-    public function checkExtension()
-    {
-
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-
-        $mimeType = finfo_file($finfo, $this->fileName);
 
         self::dd(in_array($mimeType, $this->extensionAllowed));
 
         if (!in_array($mimeType, $this->extensionAllowed) && array_key_exists($this->getExtension(), $this->extensionAllowed)) {
             echo "Le fichier n'est pas une image";
         }
-        finfo_close($finfo);
-        return true;
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getFile()
-    {
-        return $this->file;
+        return true;
     }
 
     //FunctionCustom.php
     static function dd($value){
         die( var_dump( $value ) );
     }
-
 }
--- public/index.php
+++ public/index.php
@@ -16,43 +16,36 @@ require '../UploadController.php';
           integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        <form class="form-controls" action="" method="post" enctype="multipart/form-data">
+                <label  for="upload_file"></label>
+                <input  type="file" name="upload[]" multiple="multiple" id="upload_file"/>
+                <input class="btn btn-primary" type="submit" name="submit" value="Upload" />
+        </form>
 
-<form class="form-controls" action="" method="post" enctype="multipart/form-data">
 
-        <label class="custom-file-label" for="upload_file">Choississez un ou plusieurs fichiers !</label>
-        <input class="custom-file-input" type="file" name="fichier[]" multiple="multiple" id="upload_file"/>
-        <input class="btn btn-primary" type="submit" name="submit" value="Upload" />
+<?php if (isset($_POST['submit'])): ?>
+    <?php if (empty($_FILES['name'])):?>
+        <p class="text-danger   ">Aucun fichier envoyé</p>
 
-</form>
 
-</div>
-
+<!--//    $numberFiles = count($_FILES['upload']['name']);-->
+<!--//    $verif = new UploadController($_FILES['upload']['name'][0]);-->
+<!--//-->
+<!--//    if($numberFiles > 0) {-->
+<!--//        for($i=0; $i<$numberFiles; $i++) {-->
+<!--//            //Save the name-->
+<!--//            $nameFile = $_FILES['upload']['name'][$i];-->
+<!--//            $checkFile = new UploadController($nameFile);-->
+<!--//-->
+<!--//-->
+<!--//        }-->
+    <?php endif; ?>
+<?php endif; ?>
+    </div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
         integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php
-/*
- * To do renovyer le chemin du fichier temp a la classe Upload controller
- */
-if (isset($_POST['submit'])){
-    $numberFiles = count($_FILES['fichier']['name']);
-    $verif = new UploadController($_FILES['fichier']['name'][0]);
-    var_dump($verif->checkExtension());
-    if($numberFiles > 0) {
-        for($i=0; $i<$numberFiles; $i++) {
-            //Save the name
-            $nameFile = $_FILES['upload']['name'][$i];
-            $checkFile = new UploadController($nameFile);
-
-
-        }
-    }
-
-
-
-
-
-}
